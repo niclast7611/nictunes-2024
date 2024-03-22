e420db674f75cbf691be4f023479bb1ab79f96e2
Nic Last <nic@ripemetrics.com>
2024-03-22 16:22:32 -0700

    sidebar

---
+++ components/Sidebar.tsx
@@ -0,0 +1,55 @@
+import React from "react";
+import { AiOutlineHome, AiOutlinePlusCircle } from "react-icons/ai";
+import { HiOutlineLibrary } from "react-icons/hi";
+import { FaRss } from "react-icons/fa6";
+import { FaRegHeart } from "react-icons/fa";
+import { IoSearch } from "react-icons/io5";
+
+type Props = {};
+
+const Sidebar = (props: Props) => {
+  return (
+    <div className="text-gray-500 p-5 text-sm border-r border-gray-900">
+      <div className="space-y-4">
+        <button className="flex items-center space-x-2 hover:text-white">
+          <AiOutlineHome className="h-5 w-5" />
+          <p>Home</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <IoSearch className="h-5 w-5" />
+          <p>Search</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <HiOutlineLibrary className="h-5 w-5" />
+          <p>Your Library</p>
+        </button>
+        <hr className="border-t-[0.1px] border-gray-500" />
+        <button className="flex items-center space-x-2 hover:text-white">
+          <AiOutlinePlusCircle className="h-5 w-5" />
+          <p>Create Playlist</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <FaRegHeart className="h-5 w-5" />
+          <p>Liked Songs</p>
+        </button>
+        <button className="flex items-center space-x-2 hover:text-white">
+          <FaRss className="h-5 w-5" />
+          <p>Your Episodes</p>
+        </button>
+        <hr className="border-t-[0.1px] border-gray-500" />
+
+        {/* Playlists */}
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+        <p className="cursor-pointer hover:text-white">Playlist name...</p>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,19 @@
+import Sidebar from "@/components/Sidebar";
 import React from "react";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <div className="bg-black h-screen overflow-hidden">
+      <main className="">
+        {/* sidebar */}
+        <section>
+          <Sidebar />
+        </section>
+        {/* center */}
+      </main>
+      <div>{/* player */}</div>
+    </div>
+  );
 };
 
 export default Home;
